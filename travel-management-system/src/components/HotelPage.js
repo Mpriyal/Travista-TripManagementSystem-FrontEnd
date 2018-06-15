5b48b072183e7427cb70dd216519686fc8948126
Ayush Shukla <ashukla2705@gmail.com>
2018-06-15 12:07:37 -0400

    sandbox and geocode api integrated

--- travel-management-system/src/components/HotelPage.js
+++ travel-management-system/src/components/HotelPage.js
@@ -1,162 +1,106 @@
 import React, {Component} from 'react'
-import axios from 'axios';
 import DatePicker from 'react-datepicker';
-var parseString = require('xml2js').parseString;
-var hotelUrl = 'http://api.sandbox.amadeus.com/v1.2/hotels/search-circle';
-var hotelApiKey = 'Vz8AI97I24134gBAQgl0w5oQn0TUtFzA';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+import HotelService from '../services/HotelService.js'
+
 
 export default class HotelPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            hotel: {
-                name: '',
-                city: '',
-                state: '',
-                country: '',
-                checkIn: '',
-                checkOut: '',
-                radius: ''
-            },
-            hotels: [],
-            selectedOption: 'name'
+            latitude: '42.3398198',
+            longitude: '-71.0875516',
+            checkIn: moment('2018-06-01'),
+            checkOut: moment('2018-06-11'),
+            radius: '10',
+            inputText: '',
+            hotels: []
         };
-        this.setOption = this.setOption.bind(this);
-        this.searchHotel = this.searchHotel.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.inputTextChanged = this.inputTextChanged.bind(this);
+        this.findAllHotels = this.findAllHotels.bind(this);
+        this.findAllHotelsByLatLong = this.findAllHotelsByLatLong.bind(this);
+        this.checkInDateChange = this.checkInDateChange.bind(this);
+        this.checkOutDateChange = this.checkOutDateChange.bind(this);
+        this.hotelService = HotelService.instance;
     }
 
-    handleChange(date) {
+    checkInDateChange(date) {
         this.setState({
-            startDate: date
+            checkIn : date
         });
     }
 
-    setOption(changedOption) {
-        this.setState({selectedOption: changedOption.target.value});
+    inputTextChanged(event) {
+        this.setState({
+            inputText: event.target.value
+        });
     }
-    // http://api.sandbox.amadeus.com/v1.2/hotels/search-circle?
-    //     // latitude=43.6&longitude=7.2&radius=50&check_in=2015-09-01&
-    //     // check_out=2015-09-03&chain=RT¤cy=EUR&number_of_results=50&
-    //     // apikey=<your API key>
 
-    searchHotel() {
-        var textValue = this.refs.searchValue.value;
-        console.log(textValue);
-        var currentState = this.state.selectedOption;
-        if (currentState === 'name') {
-            this.setState({hotel: {name: textValue}})
-            axios.get(hotelUrl,{
-                params:{
-                    apikey:hotelApiKey,
-                    name:this.state.hotel.name
-                }
-            })
-            console.log(this.state.hotels)
-        }
-        else if (currentState === 'city') {
-            this.setState({hotel: {city: textValue}})
-            axios.get(hotelUrl,{
-                params:{
-                    apikey:hotelApiKey,
-                    city:this.state.hotel.city
-                }
-            }).then(res=>{
-                var xml = "<root>Hello xml2js!</root>"
-                parseString(xml, function (err, result) {
-                    console.dir(result);
-                    console.log(res)
-                });
-            })
-        }
-        else if (currentState === 'state') {
-            this.setState({hotel: {state: textValue}})
-            axios.get(hotelUrl,{
-                params:{
-                    apikey:hotelApiKey,
-                    state:this.state.hotel.state
-                }
-            }).then(res=>{
-                var xml = "<root>Hello xml2js!</root>"
-                parseString(xml, function (err, result) {
-                    console.dir(result);
-                    console.log(res)
-                });
-            })
-        }
-        else if (currentState === 'country') {
-            this.setState({hotel: {country: textValue}})
-            axios.get(hotelUrl,{
-                params:{
-                    apikey:hotelApiKey,
-                    country:this.state.hotel.country
-                }
-            }).then(res=>{
-                var xml = "<root>Hello xml2js!</root>"
-                parseString(xml, function (err, result) {
-                    console.dir(result);
-                    console.log(res)
-                });
-            })
-        }
+    checkOutDateChange(date) {
+        this.setState({
+            checkOut : date
+        });
     }
+
+    findAllHotels(){
+        this.hotelService
+            .findLatLongOfHotel(this.state.inputText)
+            .then((results) => {
+                this.setLatLong(results); })
+            .then(() => this.findAllHotelsByLatLong());
+
+    }
+
+    setLatLong(results){
+        this.setState({
+            latitude:  results.results[0].geometry.location.lat,
+            longitude: results.results[0].geometry.location.lng })
+    }
+
+    findAllHotelsByLatLong() {
+        this.hotelService
+            .findAllHotelsByLatLong(this.state.latitude,
+                this.state.longitude,
+                this.state.checkIn.format("YYYY-MM-DD"),
+                this.state.checkOut.format("YYYY-MM-DD"),
+                this.state.radius)
+            .then((result) => {
+                console.log(result);
+                this.setState({
+                    hotels: result})
+            });
+    }
+
 
     render() {
         return (
             <div>
                 <div className="search">
                     <div className="input-group md-form form-sm form-2 pl-0">
-                        <input className="form-control my-0 py-1 amber-border" type="text" placeholder="Search Hotels"
-                               aria-label="Search" ref="searchValue"/>
+                        <input className="form-control my-0 py-1 amber-border"
+                               type="text"
+                               placeholder="Search Hotels"
+                               onChange={this.inputTextChanged}
+                               aria-label="Search"
+                               ref="searchValue"/>
                         <div className="input-group-append"/>
                         <span className="input-group-text amber lighten-3" id="basic-text1">
                             <i className="fa fa-search text-grey" aria-hidden="true"
-                               onClick={this.searchHotel}/>
+                               onClick={this.findAllHotels}/>
                         </span>
                     </div>
                 </div>
                 <DatePicker
+                    value = {this.state.checkIn}
                     selected={this.state.checkIn}
-                    onChange={this.handleChange}
+                    onChange={this.checkInDateChange}
                 />
-                <div className='radioSelector'>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input"
-                               type="radio" name="inlineRadioOptions"
-                               id="inlineRadio1" value="name"
-                               checked={this.state.selectedOption === 'name'}
-                               onChange={this.setOption}/>
-                        <label className="form-check-label"
-                               htmlFor="inlineRadio1">Name</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input"
-                               type="radio" name="inlineRadioOptions"
-                               id="inlineRadio1" value="city"
-                               checked={this.state.selectedOption === 'city'}
-                               onChange={this.setOption}/>
-                        <label className="form-check-label"
-                               htmlFor="inlineRadio1">City</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input"
-                               type="radio" name="inlineRadioOptions"
-                               id="inlineRadio1" value="state"
-                               checked={this.state.selectedOption === 'state'}
-                               onChange={this.setOption}/>
-                        <label className="form-check-label"
-                               htmlFor="inlineRadio1">State</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input"
-                               type="radio" name="inlineRadioOptions"
-                               id="inlineRadio1" value="country"
-                               checked={this.state.selectedOption === 'country'}
-                               onChange={this.setOption}/>
-                        <label className="form-check-label"
-                               htmlFor="inlineRadio1">Country</label>
-                    </div>
-                </div>
+                <DatePicker
+                    value = {this.state.checkOut}
+                    selected={this.state.checkOut}
+                    onChange={this.checkOutDateChange}
+                />
             </div>
         )
     }
---
+++ travel-management-system/src/services/HotelService.js
@@ -0,0 +1,42 @@
+let _singleton = Symbol();
+const HOTEL_URL = 'http://api.sandbox.amadeus.com/v1.2/hotels/search-circle?';
+const LAT_LONG_URL = 'https://maps.googleapis.com/maps/api/geocode/json?';
+const API_KEY = 'Vz8AI97I24134gBAQgl0w5oQn0TUtFzA';
+const LAT_LONG_API_KEY = 'AIzaSyCGFcq0Kr1hQAULOY9_O3azu2N4Srn-tmY'
+
+
+class HotelService {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Cannot instantiate directly.');
+    }
+    static get instance() {
+        if(!this[_singleton])
+            this[_singleton] = new HotelService(_singleton);
+        return this[_singleton]
+    }
+
+    findAllHotelsByLatLong(latitude,longitude,checkIn,checkOut,radius){
+        return fetch(HOTEL_URL +
+            'latitude=' + latitude +
+            '&longitude=' + longitude +
+            '&check_in=' + checkIn +
+            '&check_out=' + checkOut +
+            '&radius=' + radius +
+            '&apikey=' + API_KEY)
+            .then(function(response){
+                return response.json();
+            });
+    }
+
+    findLatLongOfHotel(address){
+        return fetch(LAT_LONG_URL +
+            'address=' + address +
+            '&key=' + LAT_LONG_API_KEY)
+            .then(function(response){
+                return response.json();
+            });
+    }
+
+}
+export default HotelService;
