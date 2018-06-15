7faba6e33f7745ccbeac35f3c03ad08f61489984
Priyal Mittal <priyalmittal@Priyals-MacBook-Pro.local>
2018-06-15 19:45:48 -0400

    Fixed bugs

--- travel-management-system/src/components/NavBar.js
+++ travel-management-system/src/components/NavBar.js
@@ -34,34 +34,14 @@ export default class NavBar extends Component {
                                     </Link>
                                 </li>
                                 <li className="nav-item">
-                                    <Link to='/flights'>
-                                        <div className="nav-link" href="#">Flights</div>
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
                                     <Link to='/attractions'>
                                         <div className="nav-link" href="#">Attractions</div>
                                     </Link>
                                 </li>
-                                <li className="nav-item">
-                                    <Link to='/blogs'>
-                                        <div className="nav-link" href="#">Travel Blogs</div>
-                                    </Link>
-                                </li>
-                                {/*<li className="nav-item dropdown">*/}
-                                {/*<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"*/}
-                                {/*data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">*/}
-                                {/*Dropdown*/}
-                                {/*</a>*/}
-                                {/*<div className="dropdown-menu" aria-labelledby="navbarDropdown">*/}
-                                {/*<a className="dropdown-item" href="#">Action</a>*/}
-                                {/*<a className="dropdown-item" href="#">Another action</a>*/}
-                                {/*<div className="dropdown-divider"></div>*/}
-                                {/*<a className="dropdown-item" href="#">Something else here</a>*/}
-                                {/*</div>*/}
-                                {/*</li>*/}
                                 {/*<li className="nav-item">*/}
-                                {/*<a className="nav-link disabled" href="#">Disabled</a>*/}
+                                    {/*<Link to='/blogs'>*/}
+                                        {/*<div className="nav-link" href="#">Travel Blogs</div>*/}
+                                    {/*</Link>*/}
                                 {/*</li>*/}
                             </ul>
                             <div className="float-right">
@@ -74,17 +54,8 @@ export default class NavBar extends Component {
                                     {/*// onClick={this.OnSignin}>Sign In*/}
                                 </button>
                             </div>
-                            {/*<form className="form-inline my-2 my-lg-0">*/}
-                            {/*<input className="form-control mr-sm-2" type="search" placeholder="Search"*/}
-                            {/*aria-label="Search"/>*/}
-                            {/*<button className="btn btn-outline-success my-2 my-sm-0" type="submit">*/}
-                            {/*Search*/}
-                            {/*</button>*/}
-                            {/*</form>*/}
                         </div>
                     </nav>
-                    {/*<section style={ backgroundImage }>*/}
-                    {/*</section>*/}
                 </div>
             </div>
         )
