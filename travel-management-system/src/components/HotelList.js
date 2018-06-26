9ef9ea62d15c9cf60fc9db4631b943885c3573b8
Ayush Shukla <ashukla2705@gmail.com>
2018-06-26 02:52:09 -0400

    Coupon Redeem option implemented

--- travel-management-system/src/components/HotelList.js
+++ travel-management-system/src/components/HotelList.js
@@ -18,7 +18,8 @@ export default class HotelList extends Component {
             hotels: [],
             dbHotels: [],
             coupons: [],
-            hidden: false
+            hidden: false,
+            disabled: ''
         }
 
         this.checkUserStatus = this.checkUserStatus.bind(this);
@@ -26,7 +27,38 @@ export default class HotelList extends Component {
         this.userService = UserService.instance
         this.findCurrentUserStatus()
         this.findCouponByHotelId = this.findCouponByHotelId.bind(this)
+        this.findAllCoupons = this.findAllCoupons.bind(this)
+        this.setCoupons = this.setCoupons.bind(this)
+        this.deleteCoupon = this.deleteCoupon.bind(this)
+    }
 
+    setCoupons(cpns){
+        this.setState({
+            coupons: cpns
+        })
+    }
+
+    findAllCoupons(){
+        this.couponService
+            .findAllCoupons()
+            .then(response => {this.setCoupons(response)})
+    }
+
+    deleteCoupon(couponId) {
+            this.couponService
+                .deleteCoupon(couponId)
+                .then(() => {
+                        this.findAllCoupons()
+                    }
+                );
+    }
+
+    componentDidMount(){
+        this.findAllCoupons()
+    }
+
+    componentWillReceiveProps(){
+        this.findAllCoupons()
     }
 
     findCurrentUserStatus(){
@@ -50,118 +82,79 @@ export default class HotelList extends Component {
     }
 
     render() {
-        if (this.props.data2) {
-            if (this.props.data) {
-                return (
-                    <div className="container-fluid">
-                        <div className="container p-5 m-5">
-                            <div className="row">
-                                {this.props.data2.map((hotel, index) =>
-                                    <div className="col-sm-4" key={hotel._id} style={{marginBottom: 40}}>
-                                        <div className="card">
-                                            <img className="card-img-top"
-                                                 src="https://picsum.photos/3744/5616?image=1065"
-                                                 alt="Card image cap"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">{hotel.name}</h5>
-                                                <p className="card-text">
-                                                    <b>Address:</b> {hotel.address}
-                                                </p>
-                                                <p className="card-text">
-                                                    <b>Rate:</b> {hotel.rate}
-                                                </p>
-                                                <p className="card-text"><b>Call:</b> {hotel.phone}</p>
-                                                {this.findCouponByHotelId(hotel._id)}
-                                                {this.state.coupons.length !== 0 && this.state.hidden === true && <span>
+        return (
+            <div className="container-fluid">
+                <div className="container p-5 m-5">
+                    <div className="row">
+                        {this.props.data2.map((hotel, index) =>
+                            <div className="col-sm-4" key={hotel._id} style={{marginBottom: 40}}>
+                                <div className="card">
+                                    <img className="card-img-top"
+                                         src="https://picsum.photos/3744/5616?image=1065"
+                                         alt="Card image cap"/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{hotel.name}</h5>
+                                        <p className="card-text">
+                                            <b>Address:</b> {hotel.address}
+                                        </p>
+                                        <p className="card-text">
+                                            <b>Rate:</b> {hotel.rate}
+                                        </p>
+                                        <p className="card-text"><b>Call:</b> {hotel.phone}</p>
+                                        {this.state.hidden === false &&
+                                        <span>
+                                            <Link to='/login'>
+                                                 <div className="nav-link">Login to view special discount coupons</div>
+                                            </Link>
+                                        </span>}
+                                        {this.state.coupons.map((coupon, index) =>
+                                            <div>
+                                                {coupon.hotel === hotel._id && this.state.hidden === true &&
+                                                <span>
                                                     <h6>Avail Coupons</h6>
                                                         <ul>
-                                                            {this.state.coupons.map((coupon, index) =>
-                                                                <div>
-                                                                    <li>Code: <b>{coupon.code}</b></li>
-                                                                    <li>Value: <b>{coupon.value}</b></li>
-                                                                </div>
-                                                            )}
+                                                            <li>Code: <b>{coupon.code}</b></li>
+                                                            <li>Value: <b>{coupon.value}</b></li>
                                                         </ul>
+                                                    <button className="btn btn-primary form-control" onClick={this.deleteCoupon(coupon._id)}>
+                                                        Avail Coupon
+                                                    </button>
                                                     </span>}
-                                                {this.state.coupons.length !== 0 && this.state.hidden === false && <span>
-                                                            <Link to ="/login"><i className="fa fa-sign-in btn"/>Login to view Special coupons for booking</Link>
-                                                    </span>}
-                                                {/*{renderIf(this.state.coupons.length !== 0)(*/}
-                                                    {/*<span>*/}
-                                                        {/*<ul>*/}
-                                                            {/*{this.state.coupons.map((coupon, index) =>*/}
-                                                                {/*<div>*/}
-                                                                    {/*<li>Code: {coupon.code} Value: {coupon.value}</li>*/}
-                                                                {/*</div>*/}
-                                                            {/*)}*/}
-                                                        {/*</ul>*/}
-                                                    {/*</span>*/}
-                                                {/*)}*/}
                                             </div>
-                                        </div>
+                                        )}
                                     </div>
-                                )}
+                                </div>
                             </div>
-                        </div>
-                        <div className="container p-5 m-5">
-                            <div className="row">
-                                {this.props.data.map((hotel, index) =>
-                                    <div className="col-sm-4" key={hotel.id} style={{marginBottom: 40}}>
-                                        <div className="card">
-                                            <img className="card-img-top"
-                                                 src="https://logoobject.com/wp-content/uploads/edd/2017/09/Real-Estate-Logos-Inspiration.png"
-                                                 alt="Card image cap"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">{hotel.property_name}</h5>
-                                                <p className="card-text">
-                                                    <b>Address:</b> {hotel.address.line1} {hotel.address.city} {hotel.address.region} {hotel.address.postal_code} {hotel.address.country}
-                                                </p>
-                                                <p className="card-text">
-                                                    <b>Rate:</b> {hotel.min_daily_rate.amount} {hotel.min_daily_rate.currency}
-                                                </p>
-                                                <p className="card-text"><b>Room
-                                                    Available:</b> {hotel.rooms[0].room_type_info.room_type}</p>
-                                                <p className="card-text"><b>Call:</b> {hotel.contacts[0].detail}</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )}
-                            </div>
-                        </div>
+                        )}
                     </div>
-                )
-            }
-            else {
-                return (
-                    <div className="container-fluid">
-                        <div className="container p-5 m-5">
-                            <div className="row">
-                                {this.props.data2.map((hotel, index) =>
-                                    <div className="col-sm-4" key={hotel._id} style={{marginBottom: 40}}>
-                                        <div className="card">
-                                            <img className="card-img-top"
-                                                 src="https://picsum.photos/3744/5616?image=1065"
-                                                 alt="Card image cap"/>
-                                            <div className="card-body">
-                                                <h5 className="card-title">{hotel.name}</h5>
-                                                <p className="card-text">
-                                                    <b>Address:</b> {hotel.address}
-                                                </p>
-                                                <p className="card-text">
-                                                    <b>Rate:</b> {hotel.rate}
-                                                </p>
-                                                <p className="card-text"><b>Call:</b> {hotel.phone}</p>
-                                            </div>
-                                        </div>
+                    <div className="row">
+                        {this.props.data.map((hotel, index) =>
+                            <div className="col-sm-4" key={hotel.id} style={{marginBottom: 40}}>
+                                <div className="card">
+                                    <img className="card-img-top"
+                                         src="https://logoobject.com/wp-content/uploads/edd/2017/09/Real-Estate-Logos-Inspiration.png"
+                                         alt="Card image cap"/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{hotel.property_name}</h5>
+                                        <p className="card-text">
+                                            <b>Address:</b> {hotel.address.line1} {hotel.address.city} {hotel.address.region} {hotel.address.postal_code} {hotel.address.country}
+                                        </p>
+                                        <p className="card-text">
+                                            <b>Rate:</b> {hotel.min_daily_rate.amount} {hotel.min_daily_rate.currency}
+                                        </p>
+                                        <p className="card-text"><b>Room
+                                            Available:</b> {hotel.rooms[0].room_type_info.room_type}</p>
+                                        <p className="card-text"><b>Call:</b> {hotel.contacts[0].detail}</p>
                                     </div>
-                                )}
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
-                )
-            }
-        }
+                </div>
+            </div>
+        )
     }
+
 }
 
 
